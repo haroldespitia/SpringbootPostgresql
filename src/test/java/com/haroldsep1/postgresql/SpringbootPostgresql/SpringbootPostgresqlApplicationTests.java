4ef570f42d4b64d4b71e64ffab1dbf22dee116e0
Harold Stevens Espitia Paz <haroldsep1@gmail.com>
2020-06-14 11:00:28 -0500

    Primer commit del repositorio

---
+++ src/test/java/com/haroldsep1/postgresql/SpringbootPostgresql/SpringbootPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haroldsep1.postgresql.SpringbootPostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
